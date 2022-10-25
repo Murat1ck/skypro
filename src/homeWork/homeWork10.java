d2706006cfe2eafc15a47ccd6bce689693564ac0
Muratov <muratoff101088@gmail.com>
2022-10-25 20:47:07 +0300

    Д\з 10

--- src/homeWork/homeWork10.java
+++ src/homeWork/homeWork10.java
@@ -1,2 +1,32 @@
-package homeWork;public class homeWork10 {
+package homeWork;
+
+public class homeWork10 {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+
+    public static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.printf("ФИО сотрудника — %s \n", fullName);
+    }
+    public static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчёта - " + upperFullName);
+    }
+
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+
+
 }
+
